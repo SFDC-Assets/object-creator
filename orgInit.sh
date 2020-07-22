439a8d69fa0b90b8a60d8557516dc8ffa604d23b
mshanemc <shane.mclaughlin@salesforce.com>
2020-07-22 11:15:05 -0500

    remove snapshot stuff

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,4 @@
-sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
+sfdx shane:org:create -f config/project-scratch-def.json -d 1 -s -o creator.demo --userprefix object 
 sfdx force:source:push
 sfdx shane:user:password:set -g User -l User -p sfdx12345
 sfdx force:org:open
